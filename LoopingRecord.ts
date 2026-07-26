93b965964d3106b1a142b1ec280fe244102e0b4e
Lindsay Victoria Abbott <IzzieAugustProductions@gmail.com>
2026-07-26 03:00:27 -0400

    fix: adjust button sizes and positions in looping controls

--- LoopingRecord.ts
+++ LoopingRecord.ts
@@ -43,18 +43,18 @@ function start() {
 }
 
 function createLoopingControls() {
-  const recordButton = createUIElement.button(new Vector3(-0.3, 2.1, -2.4), new Vector3(0.45, 0.2, 0.2), Quaternion.one, "Record", Color.white, 38, Color.red, undefined);
+  const recordButton = createUIElement.button(new Vector3(-0.24, 2.05, -2.4), new Vector3(0.28, 0.12, 0.12), Quaternion.one, "Rec", Color.white, 24, Color.red, undefined);
   recordButton.rayClick.setClickFunction(() => {
     toggleRecording();
   });
 
-  const playButton = createUIElement.button(new Vector3(0.3, 2.1, -2.4), new Vector3(0.45, 0.2, 0.2), Quaternion.one, "Play", Color.white, 38, Color.green, undefined);
+  const playButton = createUIElement.button(new Vector3(0.24, 2.05, -2.4), new Vector3(0.28, 0.12, 0.12), Quaternion.one, "Play", Color.white, 24, Color.green, undefined);
   playButton.rayClick.setClickFunction(() => {
     togglePlayback();
   });
 
-  statusText = createTextLabel(new Vector3(0, 2.45, -2.4), "Ready", 36, Color.white);
-  trackCountText = createTextLabel(new Vector3(0, 2.75, -2.4), "Tracks: 0", 28, Color.lightGray);
+  statusText = createTextLabel(new Vector3(0, 2.24, -2.4), "Ready", 24, Color.white);
+  trackCountText = createTextLabel(new Vector3(0, 2.38, -2.4), "Tracks: 0", 18, Color.lightGray);
 }
 
 function createTextLabel(pos: Vector3, text: string, fontSize: number, color: Color): Entity {
