67a367b8d8623e9e54ff5bc756864239e5517e45
Lindsay Victoria Abbott <IzzieAugustProductions@gmail.com>
2026-07-26 03:18:40 -0400

    fix: adjust button sizes and status text font size in looping controls

--- LoopingRecord.ts
+++ LoopingRecord.ts
@@ -43,27 +43,27 @@ function start() {
 }
 
 function createLoopingControls() {
-  const recordButton = createUIElement.button(new Vector3(-0.42, 2.08, -2.4), new Vector3(0.24, 0.1, 0.1), Quaternion.one, "Rec", Color.white, 14, Color.red, undefined);
+  const recordButton = createUIElement.button(new Vector3(-0.42, 2.08, -2.4), new Vector3(0.24, 0.1, 0.1), Quaternion.one, "Rec", Color.white, 8, Color.red, undefined);
   recordButton.rayClick.setClickFunction(() => {
     toggleRecording();
   });
 
-  const playButton = createUIElement.button(new Vector3(-0.14, 2.08, -2.4), new Vector3(0.24, 0.1, 0.1), Quaternion.one, "Play", Color.white, 14, Color.green, undefined);
+  const playButton = createUIElement.button(new Vector3(-0.14, 2.08, -2.4), new Vector3(0.24, 0.1, 0.1), Quaternion.one, "Play", Color.white, 8, Color.green, undefined);
   playButton.rayClick.setClickFunction(() => {
     togglePlayback();
   });
 
-  const stopButton = createUIElement.button(new Vector3(0.14, 2.08, -2.4), new Vector3(0.24, 0.1, 0.1), Quaternion.one, "Stop", Color.white, 14, Color.orange, undefined);
+  const stopButton = createUIElement.button(new Vector3(0.14, 2.08, -2.4), new Vector3(0.24, 0.1, 0.1), Quaternion.one, "Stop", Color.white, 8, Color.orange, undefined);
   stopButton.rayClick.setClickFunction(() => {
     stopPlayback();
   });
 
-  const resetButton = createUIElement.button(new Vector3(0.42, 2.08, -2.4), new Vector3(0.24, 0.1, 0.1), Quaternion.one, "Reset", Color.white, 14, Color.blue, undefined);
+  const resetButton = createUIElement.button(new Vector3(0.42, 2.08, -2.4), new Vector3(0.24, 0.1, 0.1), Quaternion.one, "Reset", Color.white, 8, Color.blue, undefined);
   resetButton.rayClick.setClickFunction(() => {
     resetTracks();
   });
 
-  statusText = createTextLabel(new Vector3(0, 2.25, -2.4), "Ready", 18, Color.white);
+  statusText = createTextLabel(new Vector3(0, 2.25, -2.4), "Ready", 8, Color.white);
   trackCountText = createTextLabel(new Vector3(0, 2.38, -2.4), "Tracks: 0", 14, Color.white);
 }
 
