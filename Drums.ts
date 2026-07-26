39f8574915cade7bdc85e1b9fe7deb5a333050f3
Lindsay Victoria Abbott <IzzieAugustProductions@gmail.com>
2026-07-25 20:07:17 -0400

    Refactor drum set creation by removing floor function and adjusting pad positions

--- Drums.ts
+++ Drums.ts
@@ -8,7 +8,6 @@ import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 registerStart(start);
 
 function start() {
-  createFloor();
   createDrumSet();
 }
 
@@ -22,29 +21,15 @@ type DrumPadDefinition = {
   pitch: number;
 };
 
-function createFloor() {
-  const floor = spawnPrimitive.plane(
-    "Both",
-    new Vector3(0, -0.5, 0),
-    new Vector3(8, 8, 8),
-    Quaternion.one,
-    new Color(0.16, 0.16, 0.18),
-    1,
-    "None",
-    "Empty",
-    undefined
-  );
 
-  floor.mesh.color.set(new Color(0.2, 0.2, 0.24), 1);
-}
 
 function createDrumSet() {
   const pads: DrumPadDefinition[] = [
-    { name: "Kick", color: Color.red, pos: new Vector3(-1.2, 0.4, -2), wave: "Sine", frequency: 80, volume: 0.8, pitch: 0.95 },
-    { name: "Snare", color: Color.blue, pos: new Vector3(0, 0.4, -2), wave: "Noise", frequency: 2400, volume: 0.55, pitch: 1.05 },
-    { name: "Hi-Hat", color: Color.yellow, pos: new Vector3(1.2, 0.4, -2), wave: "Square", frequency: 1800, volume: 0.45, pitch: 1.2 },
-    { name: "Tom", color: Color.green, pos: new Vector3(-0.6, 0.4, -1), wave: "Triangle", frequency: 420, volume: 0.65, pitch: 1.0 },
-    { name: "Crash", color: Color.purple, pos: new Vector3(0.6, 0.4, -1), wave: "Sawtooth", frequency: 650, volume: 0.5, pitch: 0.92 },
+    { name: "Kick", color: Color.red, pos: new Vector3(-1.2, 1.0, -2), wave: "Sine", frequency: 80, volume: 0.8, pitch: 0.95 },
+    { name: "Snare", color: Color.blue, pos: new Vector3(0, 1.0, -2), wave: "Noise", frequency: 2400, volume: 0.55, pitch: 1.05 },
+    { name: "Hi-Hat", color: Color.yellow, pos: new Vector3(1.2, 1.0, -2), wave: "Square", frequency: 1800, volume: 0.45, pitch: 1.2 },
+    { name: "Tom", color: Color.green, pos: new Vector3(-0.6, 1.0, -1), wave: "Triangle", frequency: 420, volume: 0.65, pitch: 1.0 },
+    { name: "Crash", color: Color.purple, pos: new Vector3(0.6, 1.0, -1), wave: "Sawtooth", frequency: 650, volume: 0.5, pitch: 0.92 },
   ];
 
   pads.forEach((pad) => {
