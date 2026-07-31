70ed1efa29302a5682808a5699e6bdf73698e6ec
Lindsay Victoria Abbott <IzzieAugustProductions@gmail.com>
2026-07-31 19:58:56 -0400

    fix: correct position of C Chord Short key in createPiano function

--- Piano.ts
+++ Piano.ts
@@ -26,7 +26,7 @@ type PianoKeyDefinition = {
 function createPiano() {
     const keys: PianoKeyDefinition[] = [
         { name: "C Chord Long", color: new Color(1, 0, 0), pos: new Vector3(-3, 1.4, -2), rot: new Quaternion(0, 0, 0, 1), scale: new Vector3(0.2, 0.2, 0.4), audioFilePath: DefaultAudio.filePaths.instruments.piano.cChordLong, volume: 1, pitch: 1 },
-        { name: "C Chord Short", color: new Color(1, 0, 0), pos: new Vector3(-2.5, 1.4, -2), rot: new Quaternion(0, 0, 0, 1), scale: new Vector3(0.2, 0.2, 0.4), audioFilePath: DefaultAudio.filePaths.instruments.piano.cChordShort, volume: 1, pitch: 1 },
+        { name: "C Chord Short", color: new Color(1, 0, 0), pos: new Vector3(-2.7, 1.4, -2), rot: new Quaternion(0, 0, 0, 1), scale: new Vector3(0.2, 0.2, 0.4), audioFilePath: DefaultAudio.filePaths.instruments.piano.cChordShort, volume: 1, pitch: 1 },
         { name: "C Long", color: new Color(1, 0, 0), pos: new Vector3(-2.0, 1.4, -2), rot: new Quaternion(0, 0, 0, 1), scale: new Vector3(0.2, 0.2, 0.4), audioFilePath: DefaultAudio.filePaths.instruments.piano.cLong, volume: 1, pitch: 1 },
         { name: "C Short", color: new Color(1, 0, 0), pos: new Vector3(-1.5, 1.4, -2), rot: new Quaternion(0, 0, 0, 1), scale: new Vector3(0.2, 0.2, 0.4), audioFilePath: DefaultAudio.filePaths.instruments.piano.cShort, volume: 1, pitch: 1 },
         { name: "C Long Minor", color: new Color(1, 0, 0), pos: new Vector3(-1.0, 1.4, -2), rot: new Quaternion(0, 0, 0, 1), scale: new Vector3(0.2, 0.2, 0.4), audioFilePath: DefaultAudio.filePaths.instruments.piano.cLongMinor, volume: 1, pitch: 1 },
